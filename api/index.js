df9a60fd9a3d066b48ea1df91b20da265934e54e
Pratik Bagul <pratikbagul31@gmail.com>
2024-02-04 17:37:18 +0530

    connected to mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express'
-
+import mongoose  from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
+mongoose.connect(process.env.MONGO )
+.then(()=>{
+    console.log("connected to mongo congrates");
+})
+.catch((error)=>{
+    console.log(error);
+});
 app.listen(3000 , ()=>{
     console.log("server is running /");
 });
