1b9d2a6388031c02df61f84be48a042a4d7a750b
Sankalp Patil <sankalppatil619@gmail.com>
2024-03-15 11:54:53 +0530

    update listing api route created

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -81,5 +81,21 @@ export const createListing = async (req, res, next) => {
     
   } 
   export const updateListing=async(req,res,next) => {
-    
+    const listing = await Listing.findById(req.params.id);;
+    if(!listing){
+      return next(errorHandler(404,'Listing Not Found'));
+    }
+    if(req.user.id!== listing.userRef){
+      return next(errorHandler(401,'You can only delete your own listings!'))
+    }
+    try {
+      const updateListing=await Listing.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new:true}
+      )
+      res.status(200).json(updateListing);
+    } catch (error) {
+      next(error)
+    }
   }
